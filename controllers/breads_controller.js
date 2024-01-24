17a8f68eabadf258971864bafe1ebfef712de3ba
melanie albineda <melaniealbineda@melanies-mbp.lan>
2024-01-23 19:24:28 -0500

    updated server

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,8 +4,16 @@ const Bread = require('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-  res.send(Bread)
+  res.render('Index',
+    {
+      breads: Bread,
+      title: 'Index Page'
+    }
+  )
 })
+
+
+
 
 module.exports = breads
 
